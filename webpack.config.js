3a69a891c42f7d8d11b8563e08aeeae9e71a9a71
Prakash Poudel <prakash.poudel@clinicalink.com>
2016-09-01 10:47:32 -0400

    Added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 var webpack = require("webpack");
 var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var DEPLOY_DIR = path.resolve(__dirname, 'src/client');
+
+var PROD = JSON.parse(process.env.PROD_ENV || '0');
 
 var config = {
 	entry: APP_DIR + '/index.jsx',
+	devtool: 'source-map',
 	output: {
 	  path: BUILD_DIR,
-	  filename: 'bundle.js'
+//	  filename: 'bundle.js',
+	  filename: PROD ? 'bundle.min.js' : 'bundle.js'
 	},
+	plugins: PROD ? [new webpack.optimize.UglifyJsPlugin({
+		compress : {warnings: false}
+		})] : [new HtmlWebpackPlugin({
+		  title : 'Sample',
+		  filename : DEPLOY_DIR + '/index.html',
+		  template : APP_DIR + '/index.ejs'
+		})],
 	resolve: {
 	  extensions: ['', '.js', '.jsx']
 	},
@@ -18,7 +31,7 @@ var config = {
 	    {
 	      test: /\.jsx?/,
   	      include: APP_DIR,
-	      exclude: '/node_modules/',
+	      exclude: 'node_modules/',
 	      loader: 'babel',
 	      query: {
 		  presets: ['es2015','react']
